e47408fda98e5b7c48b0e9e07b6536ed955fd73e
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-22 10:56:57 +0800

    feat(hc[skip ci]): Change sos cushion block shape

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/SosBadgeCushionBlock.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/SosBadgeCushionBlock.java
@@ -2,10 +2,23 @@ package com.haruhifanclub.mods.haruhicore.common.block.impl;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class SosBadgeCushionBlock extends Block {
+
     public SosBadgeCushionBlock() {
         super(AbstractBlock.Properties.of(Material.STONE));
+    }
+
+    private static VoxelShape shape = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return shape;
     }
 }
