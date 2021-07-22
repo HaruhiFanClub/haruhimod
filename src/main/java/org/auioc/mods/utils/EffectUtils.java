71a56a200c93e8fbd1d799697f8227f1022d4b86
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-22 21:53:46 +0800

    feat(utils): Add some effect utils

---
+++ src/main/java/org/auioc/mods/utils/EffectUtils.java
@@ -0,0 +1,15 @@
+package org.auioc.mods.utils;
+
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.Effect;
+
+public interface EffectUtils {
+    static Effect getEffect(int id) {
+        return Effect.byId(id);
+    }
+
+    static void addEffect(LivingEntity entity, int id, int duration, int amplifier) {
+        entity.addEffect(new EffectInstance(getEffect(id), duration, amplifier, true, true));
+    }
+}
