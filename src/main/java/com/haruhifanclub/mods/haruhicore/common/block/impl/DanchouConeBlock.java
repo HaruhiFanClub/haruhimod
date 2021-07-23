a2a8aa572b76d41314ff11e9f3c35df922dabc75
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-23 17:05:11 +0800

    feat(hc): Change danchou_cone block model and texture

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
@@ -2,13 +2,25 @@ package com.haruhifanclub.mods.haruhicore.common.block.impl;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class DanchouConeBlock extends Block {
+    private static final VoxelShape SHAPE = Block.box(6, 0, 6, 10, 8, 10);
+
     public DanchouConeBlock() {
         super(
             AbstractBlock.Properties
                 .of(Material.HEAVY_METAL)
         );
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
     }
 }
