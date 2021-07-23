4e61dcf1534399061b38ef8b84949b62f4e9c6b5
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-24 00:08:57 +0800

    feat(hc): Right click to place the danchou_cone on the head

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
@@ -3,16 +3,26 @@ package com.haruhifanclub.mods.haruhicore.common.block.impl;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class DanchouConeBlock extends Block {
     public static final DirectionProperty FACING = HorizontalBlock.FACING;
@@ -41,5 +51,42 @@ public class DanchouConeBlock extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    public ActionResultType use(
+        BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit
+    ) {
+        if (world.isClientSide) {
+            return ActionResultType.PASS;
+        }
+
+        if (!player.isSteppingCarefully()) {
+            return ActionResultType.PASS;
+        }
+
+        Item coneItem = this.getBlock().asItem();
+
+        PlayerInventory inv = player.inventory;
+
+        ItemStack headItemStack = inv.armor.get(3);
+        if (headItemStack.equals(ItemStack.EMPTY)) {
+            // * pass
+        } else if (headItemStack.getItem().equals(coneItem)) {
+            return ActionResultType.PASS;
+        } else {
+            int freeSlot = inv.getFreeSlot();
+            if (freeSlot == -1) { // Player has no free slots.
+                return ActionResultType.FAIL;
+            }
+
+            player.setSlot(freeSlot, headItemStack); // Move original head itemstack to free slot.
+        }
+
+        player.setItemSlot(EquipmentSlotType.HEAD, new ItemStack(coneItem));
+
+        world.setBlock(pos, Blocks.AIR.defaultBlockState(), 0); // Remove clicked cone block.
+
+        return ActionResultType.SUCCESS;
     }
 }
