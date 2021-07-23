338957c232adcfb6fa325fe898797183e8250758
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-23 12:01:41 +0800

    feat(hc): blockstate and model of sos_badge_slab

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/SosBadgeSlabBlock.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/SosBadgeSlabBlock.java
@@ -3,22 +3,54 @@ package com.haruhifanclub.mods.haruhicore.common.block.impl;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.SlabType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class SosBadgeSlabBlock extends Block {
 
+    public static final EnumProperty<SlabType> TYPE = BlockStateProperties.SLAB_TYPE;
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+
+    private static final VoxelShape BOTTOM_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    private static final VoxelShape TOP_AABB = Block.box(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+
     public SosBadgeSlabBlock() {
-        super(AbstractBlock.Properties.of(Material.HEAVY_METAL));
+        super(
+            AbstractBlock.Properties
+                .of(Material.HEAVY_METAL)
+        );
+        this.registerDefaultState(
+            this.defaultBlockState()
+                .setValue(TYPE, SlabType.BOTTOM)
+                .setValue(FACING, Direction.NORTH)
+        );
     }
 
-    private static VoxelShape shape = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(TYPE, FACING);
+    }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return shape;
+        SlabType slabtype = state.getValue(TYPE);
+        switch (slabtype) {
+            case DOUBLE:
+                return VoxelShapes.block();
+            case TOP:
+                return TOP_AABB;
+            default:
+                return BOTTOM_AABB;
+        }
     }
 }
