319463c785c832482444ae2145e30f22235b231c
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-23 17:22:28 +0800

    feat(hc): BlockState facing of danchou_cone block

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/block/impl/DanchouConeBlock.java
@@ -3,13 +3,19 @@ package com.haruhifanclub.mods.haruhicore.common.block.impl;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 public class DanchouConeBlock extends Block {
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+
     private static final VoxelShape SHAPE = Block.box(6, 0, 6, 10, 8, 10);
 
     public DanchouConeBlock() {
@@ -17,6 +23,14 @@ public class DanchouConeBlock extends Block {
             AbstractBlock.Properties
                 .of(Material.HEAVY_METAL)
         );
+        this.registerDefaultState(
+            this.defaultBlockState()
+                .setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
