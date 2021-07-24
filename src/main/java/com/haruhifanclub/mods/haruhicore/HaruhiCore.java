22f7fc91c27a9fc23d9445c8758d655eeef7458e
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-24 21:59:17 +0800

    refactor(hc): Event register

--- src/main/java/com/haruhifanclub/mods/haruhicore/HaruhiCore.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/HaruhiCore.java
@@ -1,11 +1,12 @@
 package com.haruhifanclub.mods.haruhicore;
 
+import com.haruhifanclub.mods.haruhicore.common.config.CommonConfig;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import com.haruhifanclub.mods.haruhicore.common.config.CommonConfig;
 
 @Mod(HaruhiCore.MOD_ID)
 public class HaruhiCore {
@@ -14,18 +15,19 @@ public class HaruhiCore {
     public HaruhiCore() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.CONFIG);
 
-        // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // MinecraftForge.EVENT_BUS.register(this);
-
-
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
+        modSetup(modEventBus);
+        forgeSetup(forgeEventBus);
+    }
+
+    private void modSetup(final IEventBus modEventBus) {
         com.haruhifanclub.mods.haruhicore.common.item.ItemManager.ITEMS.register(modEventBus);
         com.haruhifanclub.mods.haruhicore.common.block.BlockManager.BLOCKS.register(modEventBus);
     }
 
-    // private void setup(final FMLCommonSetupEvent event) {
-    // Loggers.info("[Init] Mod HaruhiCore is initialized");
-
-    // }
+    private void forgeSetup(final IEventBus forgeEventBus) {
+        forgeEventBus.register(com.haruhifanclub.mods.haruhicore.server.event.ServerEventHandler.class);
+    }
 }
