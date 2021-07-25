05f2120e605ea1973946ca904c9f797f4ddc422e
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-26 01:34:38 +0800

    feat(utils): give item to player

---
+++ src/main/java/org/auioc/mods/utils/ItemUtils.java
@@ -0,0 +1,16 @@
+package org.auioc.mods.utils;
+
+import javax.annotation.Nullable;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+
+public interface ItemUtils {
+    static ItemStack createItemStack(Item item, @Nullable CompoundNBT nbt, int count) {
+        ItemStack itemStack = new ItemStack(item, count);
+        if (nbt != null) {
+            itemStack.setTag(nbt);
+        }
+        return itemStack;
+    }
+}
