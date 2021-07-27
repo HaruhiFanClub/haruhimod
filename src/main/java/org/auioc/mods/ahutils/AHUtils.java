c4f5382afde0c235faf7303bdf688c6151e1c0e4
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-27 20:40:49 +0800

    refactor: items and block

--- src/main/java/org/auioc/mods/ahutils/AHUtils.java
+++ src/main/java/org/auioc/mods/ahutils/AHUtils.java
@@ -1,16 +1,15 @@
 package org.auioc.mods.ahutils;
 
-
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(AHUtils.MOD_ID)
-public class AHUtils {
+@Mod(AhUtils.MOD_ID)
+public class AhUtils {
     public static final String MOD_ID = "ahutils";
 
-    public AHUtils() {
+    public AhUtils() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
@@ -19,6 +18,8 @@ public class AHUtils {
     }
 
     private void modSetup(final IEventBus modEventBus) {
+        org.auioc.mods.ahutils.common.item.ItemManager.ITEMS.register(modEventBus);
+        org.auioc.mods.ahutils.common.block.BlockManager.BLOCKS.register(modEventBus);
         modEventBus.register(org.auioc.mods.ahutils.common.network.PacketHandler.class);
     }
 
