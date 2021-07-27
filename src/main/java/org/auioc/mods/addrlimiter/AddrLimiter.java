34cc22391aaf1ee055e3fa74c8fea0d585d62953
WakelessSloth56 <wsloth56@foxmail.com>
2021-07-27 22:26:06 +0800

    refactor(addrlimiter): remove useless log messages

--- src/main/java/org/auioc/mods/addrlimiter/AddrLimiter.java
+++ src/main/java/org/auioc/mods/addrlimiter/AddrLimiter.java
@@ -1,13 +1,9 @@
 package org.auioc.mods.addrlimiter;
 
 import org.auioc.mods.addrlimiter.config.Config;
-import org.auioc.mods.ahutils.utils.Loggers;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AddrLimiter.MOD_ID)
 public class AddrLimiter {
@@ -15,13 +11,5 @@ public class AddrLimiter {
 
     public AddrLimiter() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        Loggers.info("[Init] Mod AddrLimiter is initialized");
     }
 }
