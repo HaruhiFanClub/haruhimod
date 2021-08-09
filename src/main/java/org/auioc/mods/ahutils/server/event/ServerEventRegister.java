bb8eb13ec800c6cb0c641bfcea81340ba8c9f86e
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-09 21:34:41 +0800

    fix: remove invalid dist annotation

--- src/main/java/org/auioc/mods/ahutils/server/event/ServerEventRegister.java
+++ src/main/java/org/auioc/mods/ahutils/server/event/ServerEventRegister.java
@@ -3,11 +3,8 @@ package org.auioc.mods.ahutils.server.event;
 import org.auioc.mods.ahutils.server.event.impl.ServerLoginEvent;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.handshake.client.CHandshakePacket;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 
-@OnlyIn(Dist.DEDICATED_SERVER)
 public class ServerEventRegister {
     public static void registerServerLoginEvent(final CHandshakePacket packet, final NetworkManager manager) {
         MinecraftForge.EVENT_BUS.post(new ServerLoginEvent(packet, manager));
