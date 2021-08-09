041e43b2550e9364ce33cc085717b5f86039f032
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-09 19:32:47 +0800

    feat(ahutils): register ServerLoginEvent

---
+++ src/main/java/org/auioc/mods/ahutils/server/event/impl/ServerLoginEvent.java
@@ -0,0 +1,24 @@
+package org.auioc.mods.ahutils.server.event.impl;
+
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.handshake.client.CHandshakePacket;
+import net.minecraftforge.eventbus.api.Event;
+
+public class ServerLoginEvent extends Event {
+    private final CHandshakePacket packet;
+    private final NetworkManager manager;
+
+    public ServerLoginEvent(CHandshakePacket packet, NetworkManager manager) {
+        super();
+        this.packet = packet;
+        this.manager = manager;
+    }
+
+    public CHandshakePacket getPacket() {
+        return this.packet;
+    }
+
+    public NetworkManager getManager() {
+        return this.manager;
+    }
+}
