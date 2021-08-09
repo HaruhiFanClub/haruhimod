cf619c0eb81f1599cf1b40661197d7a61068bdcb
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-09 20:07:19 +0800

    feat(addrlimiter): log remote address when serverlist has pinged

--- src/main/java/org/auioc/mods/addrlimiter/event/EventHandler.java
+++ src/main/java/org/auioc/mods/addrlimiter/event/EventHandler.java
@@ -3,6 +3,8 @@ package org.auioc.mods.addrlimiter.event;
 import org.auioc.mods.addrlimiter.command.CommandRegister;
 import org.auioc.mods.addrlimiter.config.Config;
 import org.auioc.mods.addrlimiter.player.PlayerManager;
+import org.auioc.mods.ahutils.server.event.impl.ServerLoginEvent;
+import org.auioc.mods.ahutils.utils.LogUtil;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,6 +26,13 @@ public class EventHandler {
     public static void playerLoggedOut(final PlayerEvent.PlayerLoggedOutEvent event) {
         if (Config.EnableAddrLimiter.get()) {
             PlayerManager.getInstance().playerLogout(event.getPlayer());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPing(final ServerLoginEvent event) {
+        if (event.getPacket().getIntention() == net.minecraft.network.ProtocolType.STATUS) {
+            LogUtil.info(String.format("[%s] <-> InitialHandler has pinged", event.getNetworkManager().getRemoteAddress().toString()));
         }
     }
 }
