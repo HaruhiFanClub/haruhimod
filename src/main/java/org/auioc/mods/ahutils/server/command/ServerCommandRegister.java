d8141372c403051292aefe219cbd2febd440bf56
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-14 12:21:42 +0800

    feat(ahutils): register hurt command

--- src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegister.java
+++ src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegister.java
@@ -4,8 +4,11 @@ import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import org.auioc.mods.ahutils.server.command.arguments.DamageSourceArgument;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.command.arguments.GameProfileArgument;
 
 public class ServerCommandRegister {
@@ -16,7 +19,9 @@ public class ServerCommandRegister {
                     (ctx) -> {
                         return Command.SINGLE_SUCCESS;
                     }
-                ).then(
+                )
+
+                .then(
                     literal("crash")
                         .requires((commandSource) -> {
                             return commandSource.hasPermission(4);
@@ -31,6 +36,24 @@ public class ServerCommandRegister {
                                                 .executes((ctx) -> ServerCommandHandlers.triggerClientCrash(ctx, ctx.getArgument("mode", Integer.class)))
                                         )
                                 )
+                        )
+                )
+
+                .then(
+                    literal("hurt")
+                        .requires((commandSource) -> {
+                            return commandSource.hasPermission(3);
+                        })
+                        .then(
+                            argument("targets", EntityArgument.entities())
+                                .then(
+                                    argument("source", DamageSourceArgument.damageSource())
+                                        .then(
+                                            argument("damage", FloatArgumentType.floatArg(0F))
+                                                .executes(ServerCommandHandlers::hurtEntity)
+                                        )
+                                )
+
                         )
                 )
         );
