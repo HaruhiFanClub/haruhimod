6849b10d28a5df602df7be5e6c57e1e02820b722
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-17 13:20:48 +0800

    feat(coremod): spesendmessageevent-asm transformer

--- src/main/resources/coremods/ahutils/spesendmessageevent-asm.js
+++ src/main/resources/coremods/ahutils/spesendmessageevent-asm.js
@@ -3,10 +3,11 @@ function initializeCoreMod() {
     Opcodes = Java.type("org.objectweb.asm.Opcodes");
 
     InsnList = Java.type("org.objectweb.asm.tree.InsnList");
-
-    AbstractInsnNode = Java.type("org.objectweb.asm.tree.AbstractInsnNode");
+    InsnNode = Java.type("org.objectweb.asm.tree.InsnNode");
     VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
     MethodInsnNode = Java.type("org.objectweb.asm.tree.MethodInsnNode");
+    JumpInsnNode = Java.type("org.objectweb.asm.tree.JumpInsnNode");
+    LabelNode = Java.type("org.objectweb.asm.tree.LabelNode");
 
     return {
         "ServerPlayerEntity#sendMessage": {
@@ -18,8 +19,52 @@ function initializeCoreMod() {
                     "(Lnet/minecraft/util/text/ITextComponent;Lnet/minecraft/util/text/ChatType;Ljava/util/UUID;)V",
             },
             transformer: function (methodNode) {
+                instructions = methodNode.instructions;
+
+                var toInject = new InsnList();
+
+                var escape = new LabelNode();
+
+                toInject.add(new VarInsnNode(Opcodes.ALOAD, 1)); // ITextComponent p_241151_1_
+                toInject.add(new VarInsnNode(Opcodes.ALOAD, 2)); // ChatType p_241151_2_
+                toInject.add(new VarInsnNode(Opcodes.ALOAD, 3)); // UUID p_241151_3_
+                toInject.add(
+                    new MethodInsnNode(
+                        Opcodes.INVOKESTATIC,
+                        "org/auioc/mods/ahutils/server/event/ServerEventManager",
+                        "postServerPlayerEntitySendMessageEvent",
+                        "(Lnet/minecraft/util/text/ITextComponent;Lnet/minecraft/util/text/ChatType;Ljava/util/UUID;)Z",
+                        false
+                    )
+                );
+                toInject.add(new JumpInsnNode(Opcodes.IFEQ, escape));
+                toInject.add(new InsnNode(Opcodes.RETURN));
+                toInject.add(escape);
+
+                instructions.insert(instructions.getFirst(), toInject);
+
                 return methodNode;
             },
         },
     };
 }
+
+/* net.minecraft.entity.player.ServerPlayerEntity.sendMessage
+public void sendMessage(ITextComponent p_241151_1_, ChatType p_241151_2_, UUID p_241151_3_) {
+    this.connection.send(new SChatPacket(p_241151_1_, p_241151_2_, p_241151_3_), (p_241149_4_) -> {
+        // ...
+    }
+*/
+
+/* net.minecraft.entity.player.ServerPlayerEntity.sendMessage
+public void sendMessage(ITextComponent p_241151_1_, ChatType p_241151_2_, UUID p_241151_3_) {
+    // ~ INSERT BEGIN ~ //
+    if (org.auioc.mods.ahutils.server.event.ServerEventManager.postServerPlayerEntitySendMessageEvent(p_241151_1_, p_241151_2_, p_241151_3_)){
+        return;
+    }
+    // ~ INSERT END ~ //
+
+    this.connection.send(new SChatPacket(p_241151_1_, p_241151_2_, p_241151_3_), (p_241149_4_) -> {
+        // ...
+    }
+*/
