21c0b195ad9995fc5381e1283dc81a68c400dfce
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-22 12:11:11 +0800

    feat(haruhicore): YukisWizardHat special effects
    #8

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardHatItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardHatItem.java
@@ -1,10 +1,15 @@
 package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupManager;
+import org.auioc.mods.ahutils.utils.game.EffectUtils;
 import org.auioc.mods.ahutils.utils.game.HArmorMaterial;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class YukisWizardHatItem extends ArmorItem {
 
@@ -22,4 +27,22 @@ public class YukisWizardHatItem extends ArmorItem {
     }
 
     // TODO Custom armor model
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int index, boolean selected) {
+        if (world.isClientSide) {
+            return;
+        }
+
+        LivingEntity livingEntity = ((LivingEntity) entity);
+
+        ItemStack headItemStack = livingEntity.getItemBySlot(EquipmentSlotType.HEAD);
+        if ((headItemStack.getItem()).equals(this)) {
+            EffectUtils.addEffect(livingEntity, 11, 4, 0); // resistance
+            EffectUtils.addEffect(livingEntity, 23, 4, 0); // saturation
+            EffectUtils.addEffect(livingEntity, 24, 4, 0); // glowing
+            EffectUtils.addEffect(livingEntity, 26, 4, 0); // luck
+        }
+    }
+
 }
