25a3a6db4ca9419cd0da64ec1b7f3c89885cd150
WakelessSloth56 <wsloth56@foxmail.com>
2021-08-25 11:56:46 +0800

    feat(haruhicore): YukisWizardCloak inventoryTick
    #10

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardCloakItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardCloakItem.java
@@ -2,11 +2,17 @@ package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
 import com.haruhifanclub.mods.haruhicore.common.item.ItemManager;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupManager;
+import org.auioc.mods.ahutils.utils.game.EffectUtils;
 import org.auioc.mods.ahutils.utils.game.HArmorMaterial;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
 
 public class YukisWizardCloakItem extends ArmorItem {
 
@@ -27,4 +33,24 @@ public class YukisWizardCloakItem extends ArmorItem {
     }
 
     // TODO Custom armor model
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int index, boolean selected) {
+        if (world.isClientSide) {
+            return;
+        }
+
+        PlayerEntity player = ((PlayerEntity) entity);
+
+        ItemStack chestItemStack = player.getItemBySlot(EquipmentSlotType.CHEST);
+        if (!(chestItemStack.getItem()).equals(this)) {
+            return;
+        }
+
+        if (!player.hasEffect(Effects.ABSORPTION)) {
+            EffectUtils.addEffect(player, 22, 840, 2);
+        }
+
+    }
+
 }
