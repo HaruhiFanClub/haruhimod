b8336153edb2ad1f51f9acf78d3479bfcde5b1dc
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-03 23:27:51 +0800

    feat(ahutils): PistonAddBlockLineEvent

--- src/main/java/org/auioc/mods/ahutils/common/event/CommonEventRegistry.java
+++ src/main/java/org/auioc/mods/ahutils/common/event/CommonEventRegistry.java
@@ -1,4 +1,14 @@
 package org.auioc.mods.ahutils.common.event;
 
+import org.auioc.mods.ahutils.common.event.impl.PistonAddBlockLineEvent;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+
 public class CommonEventRegistry {
+    public static boolean postPistonAddBlockLineEvent(BlockState blockState, World level, BlockPos blockPos, Direction direction) {
+        return MinecraftForge.EVENT_BUS.post(new PistonAddBlockLineEvent(blockState, level, blockPos, direction));
+    }
 }
