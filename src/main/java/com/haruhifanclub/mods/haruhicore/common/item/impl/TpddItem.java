63e9de1c2d7a92ca3fe4204e6d53cd7594695b94
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-05 20:04:45 +0800

    feat(haruhicore): basic tpdd item
    #19

---
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/TpddItem.java
@@ -0,0 +1,9 @@
+package com.haruhifanclub.mods.haruhicore.common.item.impl;
+
+import com.haruhifanclub.mods.haruhicore.common.item.base.HCHourglassItem;
+
+public class TpddItem extends HCHourglassItem {
+
+    public TpddItem() {}
+
+}
