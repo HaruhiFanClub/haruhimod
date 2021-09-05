578151a506bf0a42b583443fdc49e1bd1530c686
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-05 21:44:11 +0800

    feat(ahutils): configurable item stack advanced tooltips

--- src/main/java/org/auioc/mods/ahutils/client/event/handler/TooltipEventHandler.java
+++ src/main/java/org/auioc/mods/ahutils/client/event/handler/TooltipEventHandler.java
@@ -1,5 +1,9 @@
 package org.auioc.mods.ahutils.client.event.handler;
 
+import org.auioc.mods.ahutils.client.config.ClientConfig;
+import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.text.ITextComponent;
@@ -10,7 +14,13 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 @OnlyIn(Dist.CLIENT)
 public class TooltipEventHandler {
 
+    private static Minecraft mc = Minecraft.getInstance();
+
     public static void handle(ItemTooltipEvent event) {
+        if (!ClientConfig.EnableAdvancedTooltip.get()) {
+            return;
+        }
+
         ItemStack itemStack = event.getItemStack();
 
         if (itemStack.isEmpty()) {
@@ -26,7 +36,22 @@ public class TooltipEventHandler {
     }
 
     private static void add(ItemTooltipEvent event, ITextComponent tooltip) {
+        if (ClientConfig.AdvancedTooltipOnlyOnDebug.get() && !isDebugMode()) {
+            return;
+        }
+        if (ClientConfig.AdvancedTooltipOnlyOnShift.get() && !isShiftKeyDown()) {
+            return;
+        }
         event.getToolTip().add(tooltip);
+    }
+
+    private static boolean isDebugMode() {
+        return mc.options.advancedItemTooltips;
+    }
+
+    private static boolean isShiftKeyDown() {
+        return InputMappings.isKeyDown(mc.getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT) ||
+            InputMappings.isKeyDown(mc.getWindow().getWindow(), GLFW.GLFW_KEY_RIGHT_SHIFT);
     }
 
 }
