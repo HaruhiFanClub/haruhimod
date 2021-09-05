79c46f03a3e578bf750f2d4997e354907c816906
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-05 19:49:37 +0800

    refactor(haruhicore): HCHourglassItem
    #18, #19

---
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/base/HCHourglassItem.java
@@ -0,0 +1,67 @@
+package com.haruhifanclub.mods.haruhicore.common.item.base;
+
+import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
+import org.auioc.mods.ahutils.utils.game.MCTimeUtils;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraft.world.World;
+
+public class HCHourglassItem extends Item {
+
+    public HCHourglassItem() {
+        super(
+            new Item.Properties()
+                .tab(ItemGroupRegistry.itemGroup)
+                .stacksTo(1)
+        );
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World level, PlayerEntity player, Hand hand) {
+        return super.use(level, player, hand);
+    }
+
+    protected static ITextComponent getTimeMessage(World level) {
+        long[] time = MCTimeUtils.getTime(level);
+
+        int[] formatedDayTime = MCTimeUtils.formatDayTime(time[0]);
+
+        TranslationTextComponent fmt = new TranslationTextComponent(
+            "item.haruhicore.hourglass.message.time_fmt",
+            String.format("%d", formatedDayTime[0]),
+            String.format("%02d", formatedDayTime[1]),
+            String.format("%02d", formatedDayTime[2]),
+            String.format("%02d", formatedDayTime[3])
+        );
+
+        Style style = Style.EMPTY.withHoverEvent(
+            new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                new StringTextComponent(
+                    String.format("Day Time: %s\nGame Time: %s\nReal ime: %d", time[0], time[1], time[2])
+                )
+            )
+        );
+
+        return fmt.withStyle(style);
+    }
+
+    protected static void broadcastTime(World level, PlayerEntity player) {
+        level.getServer().getPlayerList().broadcastMessage(
+            new TranslationTextComponent(
+                "item.haruhicore.hourglass.message", player.getDisplayName(), getTimeMessage(level)
+            ), ChatType.SYSTEM, Util.NIL_UUID
+        );
+    }
+
+}
--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/HourglassItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/HourglassItem.java
@@ -1,35 +1,16 @@
 package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
-import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
+import com.haruhifanclub.mods.haruhicore.common.item.base.HCHourglassItem;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.CooldownTracker;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Util;
-import net.minecraft.util.text.ChatType;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.World;
 
-public class HourglassItem extends Item {
+public class HourglassItem extends HCHourglassItem {
 
-    public static final int ticksAtMidnight = 18000;
-    public static final int ticksPerDay = 24000;
-    public static final int ticksPerHour = 1000;
-    public static final double ticksPerMinute = 1000d / 60d;
-    public static final double ticksPerSecond = 1000d / 60d / 60d;
-
-    public HourglassItem() {
-        super(
-            new Item.Properties()
-                .tab(ItemGroupRegistry.itemGroup)
-                .stacksTo(1)
-        );
-    }
+    public HourglassItem() {}
 
     @Override
     public ActionResult<ItemStack> use(World level, PlayerEntity player, Hand hand) {
@@ -39,50 +20,17 @@ public class HourglassItem extends Item {
             return ActionResult.pass(itemStack);
         }
 
-        if (level.isClientSide) {
-            return ActionResult.sidedSuccess(itemStack, true);
+        CooldownTracker cooldownTracker = player.getCooldowns();
+        if (cooldownTracker.isOnCooldown(this)) {
+            return ActionResult.pass(itemStack);
+        }
+        cooldownTracker.addCooldown(this, 1200);
+
+        if (!level.isClientSide()) {
+            super.broadcastTime(level, player);
         }
 
-        int rawDayTime = ((int) (level.getDayTime() % 2147483647L));
-        int dayTime = rawDayTime - ticksAtMidnight + ticksPerDay; // change the server time started with 0 at midnight
-        int day = dayTime / ticksPerDay;
-        int ticks = dayTime - day * ticksPerDay;
-        // int ticks = dayTime % ticksPerDay
-        // int hour = ticks / ticksPerHour;
-        // ticks -= hour * ticksPerHour;
-        // int minutes = (int) (ticks / ticksPerMinute);
-        // ticks -= minutes * ticksPerMinute;
-        // int seconds = (int) (ticks / ticksPerSecond);
-        int hour = (ticks / ticksPerHour) % 24;
-        int min = (int) (ticks / ticksPerMinute) % 60;
-        int sec = (int) (ticks / ticksPerSecond) % 60;
-
-        TranslationTextComponent fmt = new TranslationTextComponent(
-            "item.haruhicore.hourglass.message.time_fmt",
-            String.format("%d", day),
-            String.format("%02d", hour),
-            String.format("%02d", min),
-            String.format("%02d", sec)
-        );
-
-        Style style = Style.EMPTY.withHoverEvent(
-            new HoverEvent(
-                HoverEvent.Action.SHOW_TEXT,
-                new StringTextComponent(
-                    String.format("Day time: %s\nGame time: %s", rawDayTime, level.getGameTime())
-                )
-            )
-        );
-
-        ITextComponent message = new TranslationTextComponent(
-            "item.haruhicore.hourglass.message",
-            player.getDisplayName(),
-            fmt.withStyle(style)
-        );
-
-        level.getServer().getPlayerList().broadcastMessage(message, ChatType.SYSTEM, Util.NIL_UUID);
-
-        return ActionResult.sidedSuccess(itemStack, false);
+        return ActionResult.sidedSuccess(itemStack, level.isClientSide());
     }
 
 }
