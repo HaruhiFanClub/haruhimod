a191df380bee6adf33327b65cc74680db4577346
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-06 16:43:07 +0800

    feat(ahutils): EffectUtils getEffectInstance from nbt

--- src/main/java/org/auioc/mods/ahutils/utils/game/EffectUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/EffectUtils.java
@@ -1,15 +1,35 @@
 package org.auioc.mods.ahutils.utils.game;
 
-import net.minecraft.potion.EffectInstance;
+import javax.annotation.Nullable;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public interface EffectUtils {
+
+    @Nullable
     static Effect getEffect(int id) {
         return Effect.byId(id);
+    }
+
+    @Nullable
+    static Effect getEffect(String id) {
+        return ForgeRegistries.POTIONS.getValue(new ResourceLocation(id));
+    }
+
+    @Nullable
+    static EffectInstance getEffectInstance(CompoundNBT effect_nbt) {
+        if (effect_nbt.contains("id", 8) && effect_nbt.contains("duration", 3) && effect_nbt.contains("amplifier", 3)) {
+            return new EffectInstance(getEffect(effect_nbt.getString("id")), effect_nbt.getInt("duration"), effect_nbt.getInt("amplifier"), true, true);
+        }
+        return null;
     }
 
     static void addEffect(LivingEntity entity, int id, int duration, int amplifier) {
         entity.addEffect(new EffectInstance(getEffect(id), duration, amplifier, true, true));
     }
+
 }
