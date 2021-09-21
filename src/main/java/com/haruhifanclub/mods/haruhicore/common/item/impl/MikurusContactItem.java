145cca057c7ac4c8ec7e67a3a004234be535e73c
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-21 19:41:01 +0800

    feat: right click to equip contact to head slot
    #13

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
@@ -2,9 +2,17 @@ package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
 import com.haruhifanclub.mods.haruhicore.common.item.base.IBlessedItem;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class MikurusContactItem extends Item implements IBlessedItem {
+
+    public static final EquipmentSlotType equipmentSlotType = EquipmentSlotType.HEAD;
 
     public MikurusContactItem() {
         super(
@@ -12,6 +20,19 @@ public class MikurusContactItem extends Item implements IBlessedItem {
                 .tab(ItemGroupRegistry.itemGroup)
                 .stacksTo(1)
         );
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getItemInHand(hand);
+        ItemStack headItemStack = player.getItemBySlot(equipmentSlotType);
+        if (headItemStack.isEmpty()) {
+            player.setItemSlot(equipmentSlotType, itemStack.copy());
+            itemStack.setCount(0);
+            return ActionResult.sidedSuccess(itemStack, world.isClientSide());
+        } else {
+            return ActionResult.fail(itemStack);
+        }
     }
 
 }
