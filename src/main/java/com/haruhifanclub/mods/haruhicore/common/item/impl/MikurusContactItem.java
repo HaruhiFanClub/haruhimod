295cf513d56bba294cd1e67e92a423069f3a019e
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-21 22:11:08 +0800

    feat: continuous give specific effect when both mikurus contact and maid outfit are equipped
    #13

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
@@ -1,7 +1,9 @@
 package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
+import com.haruhifanclub.mods.haruhicore.common.item.ItemRegistry;
 import com.haruhifanclub.mods.haruhicore.common.item.base.IBlessedItem;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
+import org.auioc.mods.ahutils.utils.game.EffectUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -50,6 +52,14 @@ public class MikurusContactItem extends Item implements IBlessedItem {
         if (player.hasEffect(Effects.BLINDNESS)) {
             player.removeEffect(Effects.BLINDNESS);
         }
+
+        if (isMaidOutfitEquipped(player)) {
+            EffectUtils.addEffect(player, 32, 4, 0); // hero_of_the_village
+        }
+    }
+
+    private static boolean isMaidOutfitEquipped(PlayerEntity player) {
+        return (player.getItemBySlot(EquipmentSlotType.CHEST).getItem()).equals(ItemRegistry.MIKURUS_MAID_OUTFIT_ITEM.get());
     }
 
 }
