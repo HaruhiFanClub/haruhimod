3a9b58ac3b87f1480ad2fc16bada6d91794721ad
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-22 01:41:53 +0800

    feat: mikurus contact item emitLaser method
    #13

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/MikurusContactItem.java
@@ -4,19 +4,30 @@ import com.haruhifanclub.mods.haruhicore.common.item.ItemRegistry;
 import com.haruhifanclub.mods.haruhicore.common.item.base.IBlessedItem;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
 import org.auioc.mods.ahutils.utils.game.EffectUtils;
+import org.auioc.mods.ahutils.utils.game.EntityUtils;
+import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class MikurusContactItem extends Item implements IBlessedItem {
 
     public static final EquipmentSlotType equipmentSlotType = EquipmentSlotType.HEAD;
+    private static final double rayLength = 27.0D;
 
     public MikurusContactItem() {
         super(
@@ -57,6 +68,37 @@ public class MikurusContactItem extends Item implements IBlessedItem {
             EffectUtils.addEffect(player, 32, 4, 0); // hero_of_the_village
         }
     }
+
+    // @Override
+    // public void inventoryTick(ItemStack itemStack, World world, Entity entity, int slot, boolean selected) {
+    //     if (entity instanceof PlayerEntity && selected) {
+    //         emitLaser((PlayerEntity) entity);
+    //     }
+    // }
+
+    public static boolean emitLaser(PlayerEntity player) {
+        EntityRayTraceResult rayHitEntity = EntityUtils.getEntityRayTraceResult(player, rayLength);
+        if ((rayHitEntity != null) && (rayHitEntity.getEntity() instanceof LivingEntity)) {
+            rayHitEntity.getEntity().hurt(DamageSource.MAGIC, player.getHealth() * 0.3F);
+            return true;
+        }
+
+        BlockRayTraceResult rayHitBlock = EntityUtils.getBlockRayTraceResult(player, rayLength);
+        if (rayHitBlock.getType() != RayTraceResult.Type.MISS) {
+            World world = player.level;
+            BlockPos targetBlockPos = rayHitBlock.getBlockPos().relative(rayHitBlock.getDirection());
+            if (AbstractFireBlock.canBePlacedAt(world, targetBlockPos, player.getDirection())) {
+                world.setBlock(targetBlockPos, AbstractFireBlock.getState(world, targetBlockPos), 11);
+                return true;
+            }
+            return false;
+        }
+
+        return false;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void renderLaser() {}
 
     private static boolean isMaidOutfitEquipped(PlayerEntity player) {
         return (player.getItemBySlot(EquipmentSlotType.CHEST).getItem()).equals(ItemRegistry.MIKURUS_MAID_OUTFIT_ITEM.get());
