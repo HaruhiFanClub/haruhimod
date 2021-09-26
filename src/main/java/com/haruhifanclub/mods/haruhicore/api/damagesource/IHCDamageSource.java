931bcd7a7423476b9672006fe9f31756a35cf1ad
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-26 20:52:17 +0800

    feat: api IHCDamageSource

---
+++ src/main/java/com/haruhifanclub/mods/haruhicore/api/damagesource/IHCDamageSource.java
@@ -0,0 +1,9 @@
+package com.haruhifanclub.mods.haruhicore.api.damagesource;
+
+public interface IHCDamageSource {
+
+    public static final String messageKey = "death.haruhicore.";
+
+    public boolean isBlessedDamage();
+
+}
