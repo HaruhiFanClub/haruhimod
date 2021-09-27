4a5416a485dbd93c940ef3d9f94597f5c2774833
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-27 17:51:32 +0800

    feat: yukis wand right click function
    #14

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardWandItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardWandItem.java
@@ -1,8 +1,41 @@
 package com.haruhifanclub.mods.haruhicore.common.item.impl;
 
+import java.util.Collection;
 import com.haruhifanclub.mods.haruhicore.api.item.IHCBlessedItem;
 import com.haruhifanclub.mods.haruhicore.common.item.base.HCWizardWandItem;
+import org.auioc.mods.ahutils.utils.game.EntityUtils;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.world.World;
 
 public class YukisWizardWandItem extends HCWizardWandItem implements IHCBlessedItem {
+
+    private static final double rightUseLength = 6.0D;
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack wand = player.getItemInHand(hand);
+
+        EntityRayTraceResult rayHitEntity = EntityUtils.getEntityRayTraceResult(player, rightUseLength);
+        if ((rayHitEntity == null) || !(rayHitEntity.getEntity() instanceof LivingEntity)) {
+            return ActionResult.pass(wand);
+        }
+
+        if (!world.isClientSide()) {
+            LivingEntity target = (LivingEntity) rayHitEntity.getEntity();
+            Collection<EffectInstance> effects = target.getActiveEffects();
+            for (EffectInstance effect : effects) {
+                player.addEffect(effect);
+            }
+            target.removeAllEffects();
+        }
+
+        return ActionResult.sidedSuccess(wand, world.isClientSide());
+    }
 
 }
