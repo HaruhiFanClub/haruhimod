c203cfda5e2ab7c5ec7aeabb8283bf7b47c4649c
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-29 23:40:41 +0800

    feat: 眷顾概率修正1,2实现
    #28

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/EpicReinforcementStoneItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/EpicReinforcementStoneItem.java
@@ -4,9 +4,11 @@ import com.haruhifanclub.mods.haruhicore.api.item.IHCBlessedItem;
 import com.haruhifanclub.mods.haruhicore.common.item.base.IReinforcementStoneItem;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
 import org.apache.commons.lang3.RandomUtils;
+import org.auioc.mods.ahutils.utils.game.EffectUtils;
 import org.auioc.mods.ahutils.utils.game.EnchUtils;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -34,14 +36,30 @@ public class EpicReinforcementStoneItem extends Item implements IReinforcementSt
 
     @Override
     public ItemStack processEnchantment(ItemStack stack, Player player) {
+        float A = 33.0F, B = 66.0F, C = 99.0F;
+
+
+        int luckLevel = EffectUtils.getEffectLevel(player, MobEffects.LUCK);
+        A -= 0.6 * luckLevel;
+        B -= 0.6 * luckLevel;
+        C -= 0.6 * luckLevel;
+
+        if (DanchouConeBlockItem.isEquipped(player)) {
+            float n = 100 - C;
+            A -= n;
+            B -= n;
+            C -= n;
+        }
+
+
         ListTag enchantments = stack.getEnchantmentTags();
 
-        int P = RandomUtils.nextInt(1, 101);
-        if (P <= 33) {
+        float P = (float) RandomUtils.nextInt(1, 101);
+        if (P <= A) {
             EnchUtils.enchantAll(enchantments, 1);
-        } else if (P > 33 && P <= 66) {
+        } else if (P > A && P <= B) {
             EnchUtils.enchantRandom(enchantments, 1);
-        } else if (P > 66 && P <= 99) {
+        } else if (P > B && P <= C) {
             EnchUtils.enchantOne(EnchUtils.getHighestEnchantment(enchantments), 1);
         } else {
             EnchUtils.enchantAll(enchantments, 3);
