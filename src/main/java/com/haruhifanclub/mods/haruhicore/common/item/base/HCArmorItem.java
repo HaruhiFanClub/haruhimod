f7a63f79ef63b454e0bbd54b65ce9c9cf30dc328
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-30 14:06:08 +0800

    feat(model): HCArmorItem getArmorTexture

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/base/HCArmorItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/base/HCArmorItem.java
@@ -2,10 +2,14 @@ package com.haruhifanclub.mods.haruhicore.common.item.base;
 
 import com.haruhifanclub.mods.haruhicore.api.item.IHCArmorItem;
 import com.haruhifanclub.mods.haruhicore.common.itemgroup.ItemGroupRegistry;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class HCArmorItem extends ArmorItem implements IHCArmorItem {
 
@@ -31,10 +35,10 @@ public class HCArmorItem extends ArmorItem implements IHCArmorItem {
     //     return (A) getArmorModel();
     // }
 
-    // @Override
-    // @OnlyIn(Dist.CLIENT)
-    // public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-    //     return "haruhicore:textures/models/armor/" + this.material.getName() + ".png";
-    // }
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+        return "haruhicore:textures/models/armor/" + this.material.getName() + ".png";
+    }
 
 }
