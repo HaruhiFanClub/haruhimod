9671f2f1a553ffd3e0270a5fb78353a71d89cfa6
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-30 17:06:56 +0800

    refactor(client): 模型层定义注册使用总事件处理器

--- src/main/java/com/haruhifanclub/mods/haruhicore/client/model/LayerDefinitionRegistry.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/client/model/LayerDefinitionRegistry.java
@@ -1,19 +1,14 @@
 package com.haruhifanclub.mods.haruhicore.client.model;
 
-import com.haruhifanclub.mods.haruhicore.HaruhiCore;
 import com.haruhifanclub.mods.haruhicore.client.model.armor.WizardHatArmorModel;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 @OnlyIn(Dist.CLIENT)
-@Mod.EventBusSubscriber(modid = HaruhiCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class LayerDefinitionRegistry {
 
-    @SubscribeEvent
-    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+    public static void register(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(WizardHatArmorModel.LAYER_LOCATION, WizardHatArmorModel::create);
     }
 
