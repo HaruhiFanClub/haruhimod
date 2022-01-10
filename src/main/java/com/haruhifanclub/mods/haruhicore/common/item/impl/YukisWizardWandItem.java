d81ff482e88dcbb1e3c837852ea894e1c4279569
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-10 20:46:10 +0800

    feat(item): 长门有希的魔法杖伤害依赖于玩家效果数量并被套装加成
    #14

--- src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardWandItem.java
+++ src/main/java/com/haruhifanclub/mods/haruhicore/common/item/impl/YukisWizardWandItem.java
@@ -6,7 +6,9 @@ import com.haruhifanclub.mods.haruhicore.common.item.base.HCWizardWandItem;
 import org.auioc.mods.arnicalib.utils.game.EntityUtils;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -17,6 +19,7 @@ public class YukisWizardWandItem extends HCWizardWandItem implements IHCBlessedI
 
     private static final double depriveEffectLength = 6.0D;
     private static final int depriveEffectCooldown = 2 * 20;
+    private static final float baseMagicAttackDamage = 13.0F;
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
@@ -28,9 +31,9 @@ public class YukisWizardWandItem extends HCWizardWandItem implements IHCBlessedI
 
         if (player.isSteppingCarefully()) {
             return depriveEffect(wand, player);
-        } else {
-            return magicAttack(wand, player);
         }
+
+        return InteractionResultHolder.pass(wand);
     }
 
     private static InteractionResultHolder<ItemStack> depriveEffect(ItemStack wand, Player player) {
@@ -53,9 +56,22 @@ public class YukisWizardWandItem extends HCWizardWandItem implements IHCBlessedI
         return InteractionResultHolder.sidedSuccess(wand, player.level.isClientSide());
     }
 
-    private static InteractionResultHolder<ItemStack> magicAttack(ItemStack wand, Player player) {
-        // TODO Magic attack
-        return InteractionResultHolder.pass(wand);
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+        if (player.getCooldowns().isOnCooldown(this)) {
+            return true;
+        }
+        entity.hurt(DamageSource.indirectMagic(entity, player), getMagicAttackDamage(player));
+        return true;
+    }
+
+    private static float getMagicAttackDamage(Player player) {
+        float bonus = 2.718F;
+        if (YukisWizardHatItem.isEquipped(player) && YukisWizardCloakItem.isEquipped(player)) {
+            bonus = 3.142F;
+        }
+        float damage = baseMagicAttackDamage + bonus * (float) player.getActiveEffects().size();
+        return damage >= 32.0F ? 42.0F : damage;
     }
 
 }
