b3d432827c938734f093973cc7cd902d4335a9ab
WakelessSloth56 <wsloth56@foxmail.com>
2022-07-22 01:13:37 +0800

    feat(item): maid outfit effect
    close #11

--- src/main/java/com/haruhifanclub/haruhiism/common/item/impl/MaidOutfitItem.java
+++ src/main/java/com/haruhifanclub/haruhiism/common/item/impl/MaidOutfitItem.java
@@ -1,12 +1,21 @@
 package com.haruhifanclub.haruhiism.common.item.impl;
 
+import java.util.List;
 import java.util.function.Consumer;
 import org.auioc.mcmod.arnicalib.api.game.item.HArmorMaterial;
+import org.auioc.mcmod.arnicalib.utils.game.EffectUtils;
+import org.auioc.mcmod.arnicalib.utils.game.EntityUtils;
 import com.haruhifanclub.haruhiism.client.render.armor.MaidOutfitArmorRender;
 import com.haruhifanclub.haruhiism.common.item.HMItems;
 import com.haruhifanclub.haruhiism.common.item.base.HMArmorItem;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.IItemRenderProperties;
@@ -24,6 +33,28 @@ public class MaidOutfitItem extends HMArmorItem {
                 }),
             EquipmentSlot.CHEST
         );
+    }
+
+    public static void applyEffect(ServerPlayer player) {
+        if (!isEquipped(player)) return;
+
+        int effectLevel = EffectUtils.getEffectLevel(player, MobEffects.REGENERATION);
+        if (effectLevel == 0) {
+            var aabb = (new AABB(player.blockPosition())).inflate(5.0D);
+            List<LivingEntity> list = player.level.getEntitiesOfClass(LivingEntity.class, aabb);
+            for (var living : list) {
+                if (living instanceof Player || (EntityUtils.IS_FRIENDLY.test(living))) {
+                    living.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100));
+                }
+            }
+        } else if (effectLevel == 1) {
+            player.removeEffect(MobEffects.REGENERATION);
+            player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100));
+        }
+    }
+
+    public static boolean isEquipped(Player player) {
+        return player.getItemBySlot(EquipmentSlot.CHEST).is(HMItems.MAID_OUTFIT_ITEM.get());
     }
 
     @Override
--- src/main/java/com/haruhifanclub/haruhiism/server/event/HMServerEventHandler.java
+++ src/main/java/com/haruhifanclub/haruhiism/server/event/HMServerEventHandler.java
@@ -1,7 +1,10 @@
 package com.haruhifanclub.haruhiism.server.event;
 
+import com.haruhifanclub.haruhiism.common.item.impl.MaidOutfitItem;
 import com.haruhifanclub.haruhiism.server.command.HMServerCommands;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public final class HMServerEventHandler {
@@ -9,6 +12,13 @@ public final class HMServerEventHandler {
     @SubscribeEvent
     public static void registerCommands(final RegisterCommandsEvent event) {
         HMServerCommands.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public static void onLivingDamage(final LivingDamageEvent event) {
+        if (event.getEntityLiving() instanceof ServerPlayer player) {
+            MaidOutfitItem.applyEffect(player);
+        }
     }
 
 }
